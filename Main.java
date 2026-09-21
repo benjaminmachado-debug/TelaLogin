aa8851fd3d37b15de99d93db2f2d19d4d8b0d56b
Zettai <benjaminmachado@rede.ulbra.br>
2026-09-21 02:38:38 +0000

    a

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Conexao.inicializarBanco();
+
+        TelaLogin tela = new TelaLogin();
+        tela.setVisible(true);
+    }
+}
